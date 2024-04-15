fd324b251d824880df6cfc727d2a15b63462526f
Noah Endo <noah.eyeing@gmail.com>
2024-04-15 15:52:14 +0900

    added spirograph

--- Day02-Spirograph/empty-example/sketch.js
+++ Day02-Spirograph/empty-example/sketch.js
@@ -1,47 +1,51 @@
+let r1
+let r2
+
+let a1 = 0
+let a2 = 0
+
+let a1Inc
+let a2Inc
+
+let prevX
+let prevY
+
 function setup() {
- // put setup code here
- createCanvas(400, 400, WEBGL);
- angleMode(DEGREES);
+  createCanvas(400, 400)
+  angleMode(DEGREES)
+  background(30)
+
+  r1 = random(50, 100)
+  r2 = random(50, 100)
+
+
+  a1Inc = random(0.1, 5)
+  a2Inc = random(0.1, 5)
 }
 
 function draw() {
-  // put drawing code here
-  background(30)
+  translate( width / 2, height / 2)
+  stroke(255)
 
+  for (let i = 0; i < 300; i ++) {
+      let x1 = r1 * cos(a1)
+      let y1 = r1 * sin(a1)
 
+      let x2 = x1 + r2 * cos(a2)
+      let y2 = y1 + r2 * sin(a2)
 
-  rotateX(60)
+      let r = map(sin(frameCount), -1, 1, 180, 30)
+      let g = map(cos(frameCount), -1, 1, 90, 60)
+      let b = map(tan(frameCount), -1, 1, 45, 90)
 
-  noFill()
+      stroke(r, g, b)
 
+      line(prevX, prevY, x2, y2)
 
+      prevX = x2
+      prevY = y2
 
-  let rings = Math.floor(sin(frameCount) * 50)
-  if (rings < 0) {
-    rings = rings * -1
+      a1 += a1Inc
+      a2 += a2Inc
   }
-
-  for (let i = 0; i < rings; i++) {
-
-    let r = map(sin(frameCount / 2), -1, 1, 100, 200)
-    let g = map(i, 0, 50, 100, 200)
-    let b = map(cos(frameCount), -1, 1, 200, 100)
-
-    stroke(r, g, b)
-
-    rotate(frameCount / 500)
-
-    beginShape()
-      for (let j = 0; j < 360; j += 60) {
-        let rad = i * 5
-        //let x = rad * cos(radians(j))
-        let x = rad * cos(j)
-        let y = rad * sin(j)
-        let z = sin(frameCount + i * 10) * 50
-
-        vertex(x, y, z)
-      }
-    endShape(CLOSE)
-  }
-
 }
