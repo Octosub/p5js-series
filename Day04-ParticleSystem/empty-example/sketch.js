81f12a2c929dd418b0d7068639088822de195447
Noah Endo <noah.eyeing@gmail.com>
2024-04-22 14:34:10 +0900

    implemented particles need to bug fix

--- Day04-ParticleSystem/empty-example/sketch.js
+++ Day04-ParticleSystem/empty-example/sketch.js
@@ -1,47 +1,25 @@
+let particles = []
+
+
 function setup() {
- // put setup code here
- createCanvas(400, 400, WEBGL);
- angleMode(DEGREES);
+  createCanvas(400, 400)
+  angleMode(DEGREES)
 }
 
 function draw() {
-  // put drawing code here
   background(30)
 
-
-
-  rotateX(60)
-
-  noFill()
-
-
-
-  let rings = Math.floor(sin(frameCount) * 50)
-  if (rings < 0) {
-    rings = rings * -1
+  for(let i = 0; i < 20; i++) {
+    p = new Particle()
+    particles.push(p)
   }
 
-  for (let i = 0; i < rings; i++) {
-
-    let r = map(sin(frameCount / 2), -1, 1, 100, 200)
-    let g = map(i, 0, 50, 100, 200)
-    let b = map(cos(frameCount), -1, 1, 200, 100)
-
-    stroke(r, g, b)
-
-    rotate(frameCount / 500)
-
-    beginShape()
-      for (let j = 0; j < 360; j += 60) {
-        let rad = i * 5
-        //let x = rad * cos(radians(j))
-        let x = rad * cos(j)
-        let y = rad * sin(j)
-        let z = sin(frameCount + i * 10) * 50
-
-        vertex(x, y, z)
-      }
-    endShape(CLOSE)
+  for(let i = 0; i < particles.length; i++) {
+    if (particles[i].alpha > 0) {
+      particles[i].update()
+      particles[i].show()
+    } else {
+      particles.splice(i, 1)
+    }
   }
-
 }
