d9f84712912c0dd6e6f97140fa102ff01870d9ba
Noah Endo <noah.eyeing@gmail.com>
2024-07-15 17:23:08 +0900

    fixed code for day 05

--- Day06-ASCII/empty-example/sketch.js
+++ Day06-ASCII/empty-example/sketch.js
@@ -3,17 +3,13 @@ const density = "Ñ@#W$9876543210?!abc;:+=-,._ ";
 let shin;
 
 function preload() {
-  shin = loadImage("Shinsmile-BGblack48.png");
+  shin = loadImage("Shinsmile-BGwhite96.JPG");
 }
 
 function setup() {
   noCanvas();
 
-  background(0);
-  //image(shin, 0, 0, width, height);
 
-  let w = width / shin.width;
-  let h = height / shin.height;
   shin.loadPixels();
   for (let j = 0; j < shin.height; j++ ){
     let row = '';
@@ -24,12 +20,11 @@ function setup() {
       const b = shin.pixels[pixelIndex + 2];
       const avg = (r + g + b) / 3;
       const len = density.length;
-      const charIndex = floor(map(avg, 0, 255, 0, len));
+      const charIndex = floor(map(avg, 0, 255, 0, len - 1));
       const c = density.charAt(charIndex);
-      if (c == 'Ñ') row += '&nbsp;'
+      if (c == ' ') row += '&nbsp;'
       else row += c;
     }
     createDiv(row);
-    // console.log(row);
   }
 }
