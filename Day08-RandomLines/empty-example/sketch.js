7b87bfec5008af0a715607af003a24a84d1afcc6
Noah Taiga Endo <noah.eyeing@gmail.com>
2024-11-04 14:14:15 +0900

    modify random lines

--- Day08-RandomLines/empty-example/sketch.js
+++ Day08-RandomLines/empty-example/sketch.js
@@ -4,10 +4,21 @@ function setup() {
 }
 
 function draw() {
-  background(220);
+  const r = sin(frameCount) * 100 + 100
+  const g = cos(frameCount) * 300 + 100
+  const b = tan(frameCount) * 200 + 100
+  background(b,r,g);
+  strokeWeight(1)
+
   for(i=0; i<360; i+=6){
-    let xCordinate = 200 * cos(frameCount + i) + 200
-    let yCordinate = 200 * sin(frameCount + i) + 200 
+    stroke(r,g,b)
+    let xCordinate = 200 * cos(frameCount + i) * (i*cos(PI)) + 200
+    let yCordinate = 200 * sin(frameCount + i) * (i*sin(PI)) + 200 
     line(200, 200, xCordinate, yCordinate)   
+  }
+  stroke(r,g,b,3)
+  for(i=1;i<470;i+=1){
+    strokeWeight(sin(frameCount) * i) 
+    line(0,200,400,200)
   }
 }
